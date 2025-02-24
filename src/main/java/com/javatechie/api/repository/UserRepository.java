0cf75dfee87dd312b66157ec8f1d552e8c78488a
syed <myselfmubeen123@gmail.com>
2025-02-24 10:11:49 +0530

    V24022025-first

---
+++ src/main/java/com/javatechie/api/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.javatechie.api.repository;
+
+import com.javatechie.api.entity.User;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	User findByUserId(int id);
+
+	List<User> findByName(String name);
+}
