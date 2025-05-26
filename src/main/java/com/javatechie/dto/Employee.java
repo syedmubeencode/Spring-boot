9879b03caa5d21e06fe1a7db3921d679c6afd235
syed <myselfmubeen123@gmail.com>
2025-05-26 18:24:53 +0530

    Gzip code

---
+++ src/main/java/com/javatechie/dto/Employee.java
@@ -0,0 +1,20 @@
+package com.javatechie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private Integer employeeId;
+    private String employeeName;
+    private String employeeContactNumber;
+    private String employeeAddress;
+    private String employeeGender;
+    private String employeeDepartment;
+    private String employeeSkills;
+
+    }
