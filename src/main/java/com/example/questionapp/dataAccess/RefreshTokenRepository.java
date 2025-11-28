9ec3a917e183da8982486605198c9d309fd32056
syed <myselfmubeen123@gmail.com>
2025-11-28 22:33:27 +0530

    initial-rest

--- src/main/java/com/example/questionapp/dataAccess/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.questionapp.dataAccess;
-
-import com.example.questionapp.entities.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-
-    RefreshToken findByUserId(Long userId);
-}
--- src/main/java/com/example/questionapp/requests/UpdateCommentRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.questionapp.requests;
-
-import lombok.Data;
-
-@Data
-public class UpdateCommentRequest {
-
-    private String text;
-}
